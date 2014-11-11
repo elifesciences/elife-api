67907da7e857fa963bf40daa30e7085a7964ee96
Graham Nott <gnott@starglobal.ca>
2014-11-10 18:23:08 -0800

    Simple functioning redirect.

--- src/router/views.py
+++ src/router/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -10,5 +11,12 @@ def hello_world(request):
 def redirect(dest):
     return HttpResponseRedirect(dest)
 
+@api_view(['GET'])
 def example_route(request, arg1, arg2):
-    return redirect('http://example.org/%s/%s/' % (arg1, arg2))
+    # Quick hack to get a Location redirect
+    redirect_response = redirect('http://example.org/%s/%s/' % (arg1, arg2))
+    headers = {}
+    headers['Location'] = redirect_response.url
+    return Response(
+        status=status.HTTP_302_FOUND,
+        headers=headers)
