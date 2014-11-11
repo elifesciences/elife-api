475ec9e3405add3a262fbf6c0856639cf840c3fd
Graham Nott <gnott@starglobal.ca>
2014-11-10 17:47:16 -0800

    A simple Hello, world showing up in swagger.

--- src/router/views.py
+++ src/router/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
 
 def redirect(dest):
     return HttpResponseRedirect(dest)
