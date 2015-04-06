81926523e56e57c507b6879679c2d8f09865e9da
Graham Nott <gnott@starglobal.ca>
2015-04-06 16:11:02 -0700

    Tests for parsing Glencoe metadata.

--- src/router/tests.py
+++ src/router/tests.py
@@ -159,3 +159,96 @@ class eLifeTestCase(TestCase):
         for args in passes:
             self.elf.doi = args[0]
             self.assertEqual(self.elf.get_doi_id(), args[1])
+            
+    def test_url_from_glencoe(self):
+        "test parsing Glencoe API metadata and get the URL for a media file"
+        
+        # Result of
+        # GET http://movie-usa.glencoesoftware.com/metadata/10.7554/eLife.00007
+        json_string = ('{'
+            + '"media-4":'
+            + '    {"source_href": "http://static-movie-usa.glencoesoftware.com/source/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v004.AVI",'
+            + '    "doi": "10.7554/eLife.00007.019",'
+            + '    "flv_href": "http://static-movie-usa.glencoesoftware.com/flv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v004.flv",'
+            + '    "uuid": "b8fc244a-8f29-43de-a19f-85071ca11353",'
+            + '    "title": "",'
+            + '    "video_id": "media-4",'
+            + '    "solo_href": "http://movie-usa.glencoesoftware.com/video/10.7554/eLife.00007/media-4",'
+            + '    "height": 480,'
+            + '    "ogv_href": "http://static-movie-usa.glencoesoftware.com/ogv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v004.ogv",'
+            + '    "width": 640,'
+            + '    "href": "elife00007v004.AVI",'
+            + '    "webm_href": "http://static-movie-usa.glencoesoftware.com/webm/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v004.webm",'
+            + '    "jpg_href": "http://static-movie-usa.glencoesoftware.com/jpg/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v004.jpg",'
+            + '    "duration": 45.478766999999998,'
+            + '    "mp4_href": "http://static-movie-usa.glencoesoftware.com/mp4/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v004.mp4",'
+            + '    "legend": "",'
+            + '    "size": 54879232},'
+            + '"media-1":'
+            + '    {"source_href": "http://static-movie-usa.glencoesoftware.com/source/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v001.AVI",'
+            + '     "doi": "10.7554/eLife.00007.016",'
+            + '     "flv_href": "http://static-movie-usa.glencoesoftware.com/flv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v001.flv",'
+            + '     "uuid": "c7ea590e-941c-4834-8891-8a6601d49a2f",'
+            + '     "title": "",'
+            + '     "video_id": "media-1",'
+            + '     "solo_href": "http://movie-usa.glencoesoftware.com/video/10.7554/eLife.00007/media-1",'
+            + '     "height": 480,'
+            + '     "ogv_href": "http://static-movie-usa.glencoesoftware.com/ogv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v001.ogv",'
+            + '     "width": 640,'
+            + '     "href": "elife00007v001.AVI",'
+            + '     "webm_href": "http://static-movie-usa.glencoesoftware.com/webm/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v001.webm",'
+            + '     "jpg_href": "http://static-movie-usa.glencoesoftware.com/jpg/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v001.jpg",'
+            + '     "duration": 46.579867,'
+            + '     "mp4_href": "http://static-movie-usa.glencoesoftware.com/mp4/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v001.mp4",'
+            + '     "legend": "",'
+            + '     "size": 69605376},'
+            + '"media-2":'
+            + '    {"source_href": "http://static-movie-usa.glencoesoftware.com/source/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v002.AVI",'
+            + '     "doi": "10.7554/eLife.00007.017",'
+            + '     "flv_href": "http://static-movie-usa.glencoesoftware.com/flv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v002.flv",'
+            + '     "uuid": "34ac2c91-502b-46ce-a724-9585a7d3ce67",'
+            + '     "title": "",'
+            + '     "video_id": "media-2",'
+            + '     "solo_href": "http://movie-usa.glencoesoftware.com/video/10.7554/eLife.00007/media-2",'
+            + '     "height": 480,'
+            + '     "ogv_href": "http://static-movie-usa.glencoesoftware.com/ogv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v002.ogv",'
+            + '     "width": 640,'
+            + '     "href": "elife00007v002.AVI",'
+            + '     "webm_href": "http://static-movie-usa.glencoesoftware.com/webm/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v002.webm",'
+            + '     "jpg_href": "http://static-movie-usa.glencoesoftware.com/jpg/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v002.jpg",'
+            + '     "duration": 40.006633000000001,'
+            + '     "mp4_href": "http://static-movie-usa.glencoesoftware.com/mp4/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v002.mp4",'
+            + '     "legend": "",'
+            + '     "size": 67120640},'
+            + '"media-3":'
+            + '    {"source_href": "http://static-movie-usa.glencoesoftware.com/source/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v003.AVI",'
+            + '     "doi": "10.7554/eLife.00007.018",'
+            + '     "flv_href": "http://static-movie-usa.glencoesoftware.com/flv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v003.flv",'
+            + '     "uuid": "f4d6b37d-d5d6-4d85-bf27-0925bc3b0e20",'
+            + '     "title": "",'
+            + '     "video_id": "media-3",'
+            + '     "solo_href": "http://movie-usa.glencoesoftware.com/video/10.7554/eLife.00007/media-3",'
+            + '     "height": 480,'
+            + '     "ogv_href": "http://static-movie-usa.glencoesoftware.com/ogv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v003.ogv",'
+            + '     "width": 640,'
+            + '     "href": "elife00007v003.AVI",'
+            + '     "webm_href": "http://static-movie-usa.glencoesoftware.com/webm/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v003.webm",'
+            + '     "jpg_href": "http://static-movie-usa.glencoesoftware.com/jpg/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v003.jpg",'
+            + '     "duration": 42.509132999999999,'
+            + '     "mp4_href": "http://static-movie-usa.glencoesoftware.com/mp4/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v003.mp4",'
+            + '     "legend": "",'
+            + '     "size": 53251072}}')
+            
+        doi = '10.7554/eLife.00007'
+        passes = [
+            ('elife00007v001', 'mp4', 'http://static-movie-usa.glencoesoftware.com/mp4/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v001.mp4'),
+            ('elife00007v002', 'jpg', 'http://static-movie-usa.glencoesoftware.com/jpg/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v002.jpg'),
+            ('elife00007v003', 'webm', 'http://static-movie-usa.glencoesoftware.com/webm/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v003.webm'),
+            ('elife00007v004', 'ogv', 'http://static-movie-usa.glencoesoftware.com/ogv/10.7554/102/c4dfcc6a0f187868c665d017428e6873ae4599bf/elife00007v004.ogv'),
+            ]
+        
+        for args in passes:
+            (xlink, type, url) = args
+            m = MediaFile(doi, xlink, type)
+            glencoe_url = m.get_url_from_glencoe(type, json_string)
+            self.assertEqual(glencoe_url, url)
