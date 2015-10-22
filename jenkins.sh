22c2fe3b1a1226ed7dc59369b9db700232b26fdb
Luke Skibinski <l.skibinski@elifesciences.org>
2015-10-22 15:46:58 +0100

    added pylint to the testing mix. updated the jenkins file to use the test.sh file

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-source install.sh
+set -e
 cd src/core/ && ln -sf dev_settings.py settings.py
 cd ../../
-cd src 
-./manage.py test
+source test.sh
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -e
 source install.sh
+pylint -E src/** --load-plugins=pylint_django --disable=E1103
 ./src/manage.py test src/
