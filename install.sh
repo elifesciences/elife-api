4749800c9cbe976fe1612ab6f97f8d1aa9cb8808
Luke Skibinski <l.skibinski@elifesciences.org>
2016-04-13 13:13:33 +0100

    changed install script's '-f' to '-e' which simply checks for existence. '-f' checks for 'file' and returns false if symlink

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ ! -d venv ]; then
     virtualenv --python=`which python2` venv
 fi
 source venv/bin/activate
-if [ ! -f src/core/settings.py ]; then
+if [ ! -e src/core/settings.py ]; then
     echo "no settings.py found! using the DEV settings (dev_settings.py) by default."
     cd src/core/
     ln -s dev_settings.py settings.py
