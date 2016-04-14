ebbbd72f7f60e7aaf80a72a40a138ef6bdb0cf45
Luke Skibinski <lsh@ccxx.cx>
2016-04-14 20:07:43 +0100

    single settings file wasn't added!

---
+++ src/core/settings.py
@@ -0,0 +1,109 @@
+"""generalised settings for the elife-api project. 
+
+example settings can be found in elife.cfg
+dev settings can be found in /path/to/api/dev.cfg
+
+./install.sh will create a symlink from dev.cfg -> lax.cfg if lax.cfg not found."""
+
+import os
+from os.path import join
+from datetime import datetime
+import ConfigParser as configparser
+
+SRC_DIR = os.path.dirname(os.path.dirname(__file__)) # ll: /path/to/app/src/
+PROJECT_DIR = os.path.dirname(SRC_DIR)
+
+CFG_NAME = 'app.cfg'
+DYNCONFIG = configparser.SafeConfigParser(**{
+    'allow_no_value': True,
+    'defaults': {'dir': SRC_DIR}})
+DYNCONFIG.read(join(PROJECT_DIR, CFG_NAME)) # ll: /path/to/lax/app.cfg
+
+def cfg(path, default=0xDEADBEEF):
+    try:
+        return DYNCONFIG.get(*path.split('.'))
+    except configparser.NoOptionError:
+        # given key in section hasn't been defined
+        if default == 0xDEADBEEF:
+            raise ValueError("no value set for setting at %r" % path)
+        return default
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = cfg('general.secret-key')
+
+DEBUG = cfg('general.debug')
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = cfg('general.allowed-hosts', '').split(',')
+
+# Application definition
+
+INSTALLED_APPS = (
+    #'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_markdown2',
+
+    'router',
+    
+    'rest_framework',
+    'rest_framework_swagger',
+)
+
+MIDDLEWARE_CLASSES = (
+    #'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware', #https://docs.djangoproject.com/en/dev/ref/middleware/#module-django.middleware.common
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'core.urls'
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': cfg('database.engine'),
+        'NAME': cfg('database.name'),
+        'USER': cfg('database.user'),
+        'PASSWORD': cfg('database.password'),
+        'HOST': cfg('database.host'),
+        'PORT': cfg('database.port')
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    join(SRC_DIR, 'static'),
+]
+STATIC_ROOT = join(PROJECT_DIR, 'collected-static')
+
+TEMPLATE_DIRS = [
+    join(SRC_DIR, 'templates'),
+]
